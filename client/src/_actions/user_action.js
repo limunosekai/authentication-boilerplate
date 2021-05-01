a9e5f374badd304ab5a154f878265af676b1f06e
limunosekai <powerlsh0103@gmail.com>
2021-05-01 13:35:13 +0900

    Add Client Login Func

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { LOGIN_USER } from './types';
+
+export function loginUser(dataSubmit) {
+  const request = axios
+    .post('/api/users/login', dataSubmit)
+    .then((response) => {
+      return response.data;
+    });
+
+  return {
+    type: LOGIN_USER,
+    payload: request,
+  };
+}
